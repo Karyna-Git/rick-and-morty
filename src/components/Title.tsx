d970956d3be4eeb0ce6d92de5c6a332dd40f67d3
Karyna <kharinakaryna.com>
2023-03-14 20:28:09 +0200

    created title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Title: React.FC = () => {
+  return (
+    <img
+      style={{
+        width: "100%",
+        maxWidth: "600px",
+        minWidth: "312px",
+        maxHeight: "200px",
+        minHeight: "104px",
+      }}
+      src="title.png"
+      alt="title"
+    />
+  );
+};
