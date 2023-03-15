54b840dcf641bbe9368d2564c1f7377e2bfc236b
Karyna <kharinakaryna.com>
2023-03-15 16:51:34 +0200

    updated title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,15 +1,18 @@
+import { Avatar } from "@mui/material";
 import React from "react";
 
 export const Title: React.FC = () => {
   return (
-    <img
-      style={{
+    <Avatar
+      sx={{
         width: "100%",
+        height: "100%",
         maxWidth: "600px",
         minWidth: "312px",
         maxHeight: "200px",
         minHeight: "104px",
       }}
+      variant="square"
       src="title.png"
       alt="title"
     />
