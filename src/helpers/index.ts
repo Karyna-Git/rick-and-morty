408980757252ea252071d931629324af16ec49fe
Karyna <kharinakaryna.com>
2023-03-19 18:37:00 +0200

    created sorting characters by name method

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,9 @@
+import { ICharacter } from "./../types";
+
 export const capitalizeFirstLetter = (word: string): string => {
   return word[0].toUpperCase().concat(word.slice(1));
+};
+
+export const sortCharactersByName = (characters: ICharacter[]): ICharacter[] => {
+  return characters.sort((a, b) => a.name.localeCompare(b.name));
 };
