844e097a055890a406fd4b5307eec2bb4c0d1ac5
Karyna <kharinakaryna.com>
2023-03-19 18:43:10 +0200

    updated query and response

--- src/store/api.ts
+++ src/store/api.ts
@@ -1,16 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { sortCharactersByName } from "./../helpers/index";
 import { ICharacter, ServerResponse } from "./../types";
+
+interface QueryProps {
+  page?: number;
+  search?: string;
+}
 
 export const charactersApi = createApi({
   reducerPath: "charactersApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://rickandmortyapi.com/api/character",
+    baseUrl: "https://rickandmortyapi.com/api/character/",
   }),
   endpoints: (build) => ({
-    searchCharacters: build.query<ICharacter[], string>({
-      query: (search?: string) => ({ url: search ? `/?name=${search}` : "/" }),
-      transformResponse: (response: ServerResponse<ICharacter>) =>
-        response.results,
+    searchCharacters: build.query<ServerResponse<ICharacter>, QueryProps>({
+      query: ({ page = 1, search = "" }) => ({
+        url: `/?page=${page}${search ? "&name=" + search : ""}`,
+      }),
+      transformResponse: (response: ServerResponse<ICharacter>) => ({
+        results: sortCharactersByName(response.results),
+        info: response.info,
+      }),
     }),
     getCharacterById: build.query<ICharacter, number>({
       query: (id: number) => ({ url: `/${id}` }),
