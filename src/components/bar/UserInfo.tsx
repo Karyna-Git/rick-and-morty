f1dd4427ab9c8b29c51c40a8574a7e78e9f86c77
Karyna <kharinakaryna.com>
2023-03-21 20:08:38 +0200

    added clearing of query string on logout

--- src/components/bar/UserInfo.tsx
+++ src/components/bar/UserInfo.tsx
@@ -1,5 +1,7 @@
 import { googleLogout } from "@react-oauth/google";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { charactersActions } from "../../store/characters.slice";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { userActions } from "../../store/user.slice";
 import { AvatarButton } from "./AvatarButton";
@@ -7,6 +9,7 @@ import { UserInfoMenu } from "./UserInfoMenu";
 
 export const UserInfo: React.FC = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const { name, email, picture } = useAppSelector(
     (state) => state.user.profile
@@ -28,6 +31,8 @@ export const UserInfo: React.FC = () => {
     googleLogout();
     dispatch(userActions.setProfile(null));
     dispatch(userActions.setUser(null));
+    dispatch(charactersActions.setFilter(null));
+    navigate("/");
   };
 
   return (
